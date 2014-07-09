b227ff408cd214f142484b2dcbd94c6c1817da4a
Cyril Rohr <cyril.rohr@gmail.com>
2014-07-09 17:11:44 +0000

    Very high retry attempts for curl commands.

--- lib/language_pack/fetcher.rb
+++ lib/language_pack/fetcher.rb
@@ -5,6 +5,7 @@ module LanguagePack
   class Fetcher
     include ShellHelpers
     CDN_YAML_FILE = File.expand_path("../../../config/cdn.yml", __FILE__)
+    FETCHER_RETRY = ENV.fetch('FETCHER_RETRY') { 5 }.to_i
 
     def initialize(host_url, stack = nil)
       @config   = load_config
@@ -14,22 +15,22 @@ module LanguagePack
 
     def fetch(path)
       curl = curl_command("-O #{@host_url.join(path)}")
-      run!(curl, :attempts => 3)
+      run!(curl, :attempts => FETCHER_RETRY)
     end
 
     def fetch_untar(path, files_to_extract = nil)
       curl = curl_command("#{@host_url.join(path)} -s -o")
-      run!("#{curl} - | tar zxf - #{files_to_extract}")
+      run!("#{curl} - | tar zxf - #{files_to_extract}", :attempts => FETCHER_RETRY)
     end
 
     def fetch_bunzip2(path, files_to_extract = nil)
       curl = curl_command("#{@host_url.join(path)} -s -o")
-      run!("#{curl} - | tar jxf - #{files_to_extract}")
+      run!("#{curl} - | tar jxf - #{files_to_extract}", :attempts => FETCHER_RETRY)
     end
 
     private
     def curl_command(command)
-      "set -o pipefail; curl --fail --retry 10 --connect-timeout #{curl_connect_timeout_in_seconds} --max-time #{curl_timeout_in_seconds} #{command}"
+      "set -o pipefail; curl -v --fail --retry 10 --connect-timeout #{curl_connect_timeout_in_seconds} --max-time #{curl_timeout_in_seconds} #{command}"
     end
 
     def curl_timeout_in_seconds

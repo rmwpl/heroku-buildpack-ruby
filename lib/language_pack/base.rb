9865159b32599a55b1ba0ddd02835dcf1cb540c7
Cyril Rohr <cyril.rohr@gmail.com>
2015-02-11 19:23:07 +0000

    Fix debian url

--- lib/language_pack/base.rb
+++ lib/language_pack/base.rb
@@ -24,7 +24,7 @@ class LanguagePack::Base
       when /^jessie/
         "ubuntu-14.04"
       when /^7/
-        "debian-7.4"
+        "debian-7"
       end
     elsif File.exists?("/etc/redhat-release")
       redhat_release = File.read("/etc/redhat-release").chomp

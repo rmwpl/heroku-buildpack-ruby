2d98d38bdbda2aa3a62bf27bcc4950c0ac38dab1
Cyril Rohr <cyril.rohr@gmail.com>
2015-12-04 16:00:50 +0000

    debug

--- lib/language_pack/shell_helpers.rb
+++ lib/language_pack/shell_helpers.rb
@@ -86,6 +86,9 @@ module LanguagePack
       options[:out] ||= "2>&1"
       options[:env] = user_env_hash.merge(options[:env]) if options[:user_env]
       env = options[:env].map {|key, value| "#{key.shellescape}=#{value.shellescape}" }.join(" ")
+      puts "DEBUG: #{command.inspect}"
+      puts "DEBUG: #{user_env_hash.inspect}"
+      puts "DEBUG: #{env.inspect}"
       "/usr/bin/env #{env} bash -c #{command.shellescape} #{options[:out]} "
     end
 
